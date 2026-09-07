9400ab81d185c9f89648e87e7bea47209b670fa1
Hareem-mk <noorarqum@hotmail.com>
2026-09-07 19:29:44 +0300

    Change model from mixtral to llama-3.3

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ if submitted:
                 """
 
                 response = client.chat.completions.create(
-                    model="mixtral-8x7b-32768",
+                    model="llama-3.3-70b-versatile",
                     messages=[{"role": "user", "content": prompt}],
                     temperature=0.7,
                 )
