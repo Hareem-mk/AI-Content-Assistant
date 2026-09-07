f13dc2eb6527f62a44e6ed212c44bd73eb419187
Hareem-mk <noorarqum@hotmail.com>
2026-09-07 19:16:23 +0300

    Update model version in chat completion

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ if submitted:
                 """
 
                 response = client.chat.completions.create(
-                    model="llama-3.1-8b-instant",
+                    model="llama-3.3-70b-versatile",
                     messages=[{"role": "user", "content": prompt}],
                     temperature=0.7,
                 )
